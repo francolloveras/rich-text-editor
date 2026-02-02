06155f60987e847c400dc3a1af28f18453fb638a
Franco Lloveras <francorlloveras@gmail.com>
2026-02-02 17:07:30 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import Unfonts from 'unplugin-fonts/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Geist',
+            src: './src/assets/fonts/geist.ttf'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
