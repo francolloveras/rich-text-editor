6937e4a4b6b413d255555b5eb73471b4092f1ddc
Franco Lloveras <francorlloveras@gmail.com>
2026-02-02 17:09:26 -0300

    Create SECTIONS constant

---
+++ src/lib/const.ts
@@ -0,0 +1,48 @@
+export const SECTIONS = {
+  DEFAULT: {
+    id: 'default',
+    label: 'Default'
+  },
+  DISABLED: {
+    id: 'disabled',
+    label: 'Disabled'
+  },
+  WITH_ICON: {
+    id: 'with-icon',
+    label: 'With Icon'
+  },
+  WITH_LABEL: {
+    id: 'with-label',
+    label: 'With Label'
+  },
+  WITH_ERROR: {
+    id: 'with-error',
+    label: 'With Error'
+  },
+  WITH_SUPPORTING_TEXT: {
+    id: 'with-supporting-text',
+    label: 'With Supporting Text'
+  },
+  DEFAULT_VALUE: {
+    id: 'default-value',
+    label: 'Default Value'
+  },
+  CONTROLLED: {
+    id: 'controlled',
+    label: 'Controlled'
+  },
+  FORM_VALUE: {
+    id: 'form-value',
+    label: 'Form Value'
+  },
+  DRAG_AND_DROP: {
+    id: 'drag-and-drop',
+    label: 'Drag and Drop',
+    description: 'Drag and drop the images into the rich text editor.'
+  },
+  CLIPBOARD_FILES: {
+    id: 'clipboard-files',
+    label: 'Clipboard Files',
+    description: 'Copy an image and paste into the rich text editor.'
+  }
+}
